a92663cf63788392cdb452c1743176b0a0da27b9
darcosion <darcosion@protonmail.com>
2017-10-27 14:13:37 +0200

    nouvelle version de test

--- rendu-visuel/d3tree.js
+++ rendu-visuel/d3tree.js
@@ -1,51 +1,102 @@
-var svg = d3.select("section").select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+// set the dimensions and margins of the diagram
+var margin = {top: 20, right: 180, bottom: 30, left: 180},
+  width = 660 - margin.left - margin.right,
+  height = 700 - margin.top - margin.bottom;
+
+var svg = d3.select("section").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom),
+    g = svg.append("g")
+      .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
 
-var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
+// declares a tree layout and assigns the size
+var treemap = d3.tree()
+  .size([height, width]);
 
-d3.json("d3-data.json", function(error, graph) {
+// load the external data
+d3.json("d3-data.json", function(error, treeData) {
   if (error) throw error;
 
-  var link = svg.append("g")
-      .attr("class", "links")
-    .selectAll("line")
-    .data(graph.links)
-    .enter().append("line")
-      .attr("stroke-width", function(d) { return Math.sqrt(d.value); })
-      .attr("fill", function(d) { return "#" + d.group * 31; });;
+  //  assigns the data to a hierarchy using parent-child relationships
+  nodesc = d3.hierarchy(treeData, function(d) {
+    return d.parents; //remplacer parents par children, trouver un moyen de concilier proprement les deux.
+    });
 
-  var node = svg.append("g")
-      .attr("class", "nodes")
-    .selectAll("circle")
-    .data(graph.nodes)
-    .enter().append("circle")
-      .attr("r", 5)
-      .attr("fill", function(d) { return "#" + d.group * 31; });
+  // maps the node data to the tree layout
+  nodesc = treemap(nodesc);
+  
+  //  assigns the data to a hierarchy using parent-child relationships
+  nodesp = d3.hierarchy(treeData, function(d) {
+    return d.parents;
+    });
 
-  node.append("title")
-      .text(function(d) { return d.id; });
+  // maps the node data to the tree layout
+  nodesp = treemap(nodesp);
 
-  simulation
-      .nodes(graph.nodes)
-      .on("tick", ticked);
+  // adds the links between the nodes
+  var link = g.selectAll(".link")
+    .data(nodesc.descendants().slice(1))
+    .enter().append("path")
+    .attr("class", "link")
+    .attr("d", function(d) {
+       return "M" + d.y + "," + d.x
+       + "C" + (d.y + d.parent.y) / 2 + "," + d.x
+       + " " + (d.y + d.parent.y) / 2 + "," + d.parent.x
+       + " " + d.parent.y + "," + d.parent.x;
+       });
+ 
+  // utiliser ancestors pour traiter les nodes précédents le noeud d'origine
+  // adds the links betfore the nodes
+  var link1 = g.selectAll(".link")
+    .data(nodesp.ancestors().slice(1))
+    .enter().append("path")
+    .attr("class", "link")
+    .attr("d", function(d) {
+       return "M" + d.y + "," + d.x
+       + "C" + (d.y + d.children.y) / 2 + "," + d.x
+       + " " + (d.y + d.children.y) / 2 + "," + d.children.x
+       + " " + d.children.y + "," + d.children.x;
+       });
 
-  simulation.force("link")
-      .links(graph.links);
+  // adds each node as a group
+  var node = g.selectAll(".node")
+    .data(nodesc.descendants())
+    .enter().append("g")
+    .attr("class", function(d) { 
+      return "node node--internal"; })
+    .attr("transform", function(d) { 
+      return "translate(" + d.y + "," + d.x + ")"; });
+      
+  var node1 = g.selectAll(".node")
+    .data(nodesp.ancestors())
+    .enter().append("g")
+    .attr("class", function(d) { 
+      return "node node--internal"; })
+    .attr("transform", function(d) { 
+      return "translate(" + d.y + "," + d.x + ")"; });
 
-  function ticked() {
-    link
-        .attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+  // adds the circle to the node
+  node.append("circle")
+    .attr("r", 10);
+    
+  node1.append("circle")
+    .attr("r", 10);
 
-    node
-        .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-  }
+  // adds the text to the node
+  node.append("text")
+    .attr("dy", ".35em")
+    .attr("x", function(d) { return d.children ? -13 : 13; })
+    .style("text-anchor", function(d) { 
+		return d.children ? "end" : "start"; })
+    .text(function(d) { return d.data.name; });
+    
+  node1.append("text")
+    .attr("dy", ".35em")
+    .attr("x", function(d) { return d.parents ? -13 : 13; })
+    .style("text-anchor", function(d) { 
+		return d.parents ? "end" : "start"; })
+    .text(function(d) { return d.data.name; });
+
 });
