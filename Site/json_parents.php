7e573d62d0b8f6175fb2864cda603b557e556eb3
Linda Benboudiaf <lbenboudiaf@gmail.com>
2017-11-24 17:12:46 +0100

    akhsjabsa

--- Site/json_parents.php
+++ Site/json_parents.php
@@ -7,7 +7,7 @@ header ('Content-Type: application/json');
     //$data = $bdd->query('SELECT * FROM personne');
 
 
-$data = $bdd->query('SELECT p2.nom, p2.prenom, p3.nom, p3.prenom 
+$data = $bdd->query('SELECT p1.nom, p1.prenom, p2.nom, p2.prenom, p3.nom, p3.prenom 
 FROM personne p1 , personne p2, personne p3
 where p1.pere = p2.numpers and p1.mere= p3.numpers'); // on Récupere les noms et prenoms des parents (Pere et mere)
 // On affiche chaque entrée une à une
