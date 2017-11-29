2accdf5545716d70b8d7929a19e930bd6bd0440a
darcosion <toopak@example.org>
2017-11-29 12:25:44 +0100

    finalisation graph de base

---
+++ rendu-visuel/d3lib.js
@@ -0,0 +1,19 @@
+// Library personnelle contenant toutes les fonctions optimisés 
+// de construction graphique pour ce type d'arbre particulier.
+
+// création Darcosion 2017
+
+function leftCurve(parentx, parenty, espacement, x, y) {
+	return "M" + parenty + ", " + x +
+	"C" +  (((y + parenty) / 2) + espacement) + ", " + x +
+	" " +  (((y + parenty) / 2) + espacement) + ", " + parentx +
+	" " + y + ", " + parentx;
+}
+
+
+function rightCurve(parentx, parenty, espacement, x, y) {
+	return "M" + (y*1.5) + ", " + x +
+	"C" +  (((y + parenty) / 0.9) - espacement)  + ", " + x +
+	" " +  (((y + parenty) * 1.5) - espacement)  + ", " + parentx +
+	" " + y + ", " + parentx;
+}
